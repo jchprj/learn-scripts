6c7358b22ad45e6777a8e405dc6a99259f1ae55f
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-08-10 18:34:20 +0800

    Python

---
+++ python/flow/condition.py
@@ -0,0 +1,18 @@
+# Switch Case in Python (Replacement) https://www.geeksforgeeks.org/switch-case-in-python-replacement/
+def numbers_to_strings(argument): 
+    switcher = { 
+        0: "zero", 
+        1: "one", 
+        2: "two", 
+    } 
+  
+    # get() method of dictionary data type returns  
+    # value of passed argument if it is present  
+    # in dictionary otherwise second argument will 
+    # be assigned as default value of passed argument 
+    return switcher.get(argument, "nothing") 
+  
+# Driver program 
+if __name__ == "__main__": 
+    argument=2
+    print(numbers_to_strings(argument))
---
+++ python/pandas/data_frame.py
@@ -0,0 +1,62 @@
+# Reference: https://www.geeksforgeeks.org/apply-function-to-every-row-in-a-pandas-dataframe/
+# (code changed from original)
+
+import pandas as pd 
+import numpy as np 
+  
+# Function to add 
+def add(a, b, c): 
+    return a + b + c 
+
+
+def normalize(x, y): 
+    x_new = ((x - np.mean([x, y])) /
+             (max(x, y) - min(x, y))) 
+      
+    # print(x_new) 
+    return x_new 
+
+
+def main(): 
+      
+    # create a dictionary with 
+    # three fields each 
+    data = { 
+            'A':[1, 2, 3],  
+            'B':[4, 5, 6],  
+            'C':[7, 8, 9] } 
+      
+    # Convert the dictionary into DataFrame  
+    df = pd.DataFrame(data) 
+    print("Original DataFrame:\n", df) 
+      
+    df['add'] = df.apply(lambda row : add(row['A'], 
+                     row['B'], row['C']), axis = 1) 
+   
+    print('\nAfter Applying Function: ') 
+    # printing the new dataframe 
+    print(df) 
+
+    # applying function to each row in the dataframe 
+    # and storing result in a new coloumn 
+    df['add'] = df.apply(np.sum, axis = 1) 
+   
+    print('\nAfter Applying Function: ') 
+    # printing the new dataframe 
+    print(df) 
+
+    df['X'] = df.apply(lambda row : normalize(row['A'], 
+                                  row['B']), axis = 1) 
+   
+    print('\nNormalized:') 
+    print(df) 
+
+# https://pandas.pydata.org/pandas-docs/version/0.23.3/generated/pandas.DataFrame.drop.html
+    df = df.drop(['X'], axis=1)
+   
+    print('\nDropped:') 
+    print(df) 
+
+   
+if __name__ == '__main__': 
+    main() 
