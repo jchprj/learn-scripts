3fc20467c155ba0c45be4a3a301786f96927732c
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-09-16 19:17:58 +0800

    vault

--- python/cloud/ansible/vault.py
+++ python/cloud/ansible/vault.py
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/44142208/how-to-view-decrypt-ansible-vault-credentials-files-from-within-a-python-script
+
 from ansible.constants import DEFAULT_VAULT_ID_MATCH
 from ansible.parsing.vault import VaultLib
 from ansible.parsing.vault import VaultSecret
