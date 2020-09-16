3fae827423e0ded1d1e6009fb81ec629b0272f84
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-09-16 11:42:15 +0800

    Jenkins

--- jenkins/basic/withEnv.groovy
+++ jenkins/basic/withEnv.groovy
@@ -1,7 +1,9 @@
 node {
   withEnv(['VAR=abc']) {
-    sh 'echo $VAR'
-    sh 'echo ${VAR}'
-    sh "echo ${VAR}"
+    sh 'echo line 1: $VAR'
+    sh 'echo line 2: ${VAR}'
+    sh "echo line 3: ${VAR}"
+    sh "echo line 4: ${env.VAR}"
+    sh 'echo line 5: ${env.VAR}'    //Will fail due to Bad substitution
   }
 }
