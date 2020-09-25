52d347f85e6818c3610eab0ccde48191060386de
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-09-26 02:10:02 +0800

    Jenkins

--- jenkins/basic/withEnv.groovy
+++ jenkins/basic/withEnv.groovy
@@ -1,5 +1,25 @@
 node {
   withEnv(['VAR=${WORKSPACE}/abc']) {
+    // https://stackoverflow.com/questions/60067980/jenkins-pipeline-set-environment-variable-based-on-parameter
+    switch("${VAR}") {
+      case 'abcd':
+        env.BRANCH = "master"
+        break
+      case '${WORKSPACE}/abc':
+        env.BRANCH = "2020Q1"
+        break 
+      case 'prod':
+        env.BRANCH = "2019Q4"
+        break        
+    }
+    withEnv(["BRANCH=${env.BRANCH}"]) {
+      sh 'echo BRANCH: ${BRANCH}'
+
+    }
+    sh """
+    echo line 1: $VAR
+    echo linedddddd
+    """
     sh 'echo line 1: $VAR'
     sh 'echo line 2: ${VAR}'
     sh "echo line 3: ${VAR}"
