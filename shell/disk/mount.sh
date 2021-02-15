4c729aeb97a9c60d41669dc020da7ccad7c87e11
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-02-15 12:43:23 +0800

    Shell

--- shell/disk/mount.sh
+++ shell/disk/mount.sh
@@ -15,4 +15,17 @@ mkdir temp_test.dir
 sudo mount -t drvfs '\\servername\sharename' temp_test.dir
 
 
+# https://www.raspberrypi.org/forums/viewtopic.php?t=276537
+# https://askubuntu.com/questions/427044/mounting-cifs-drive-gives-mount-error22-invalid-argument
+# tail -f  /var/log/kern.log 
+# dmesg | tail
+# https://serverfault.com/questions/222074/fstab-and-cifs-mounting-possible-to-store-authentication-information-outside-of
+sudo mount -t cifs //servername/sharename temp_test.dir -o vers="2.0",rw,soft,credentials=.credential,uid=1000,gid=1000,file_mode=0770,dir_mode=0770
 
+# https://www.raspberrypi.org/forums/viewtopic.php?t=259579
+sudo mount -t cifs //servername/sharename temp_test.dir -o vers="2.0",rw,_netdev,credentials=.credential,uid=1000,gid=1000,file_mode=0770,dir_mode=0770,x-systemd.automount
+
+
+
+# https://serverfault.com/questions/174181/how-do-you-validate-fstab-without-rebooting
+sudo mount -a
---
+++ shell/disk/umount.sh
@@ -0,0 +1,6 @@
+# https://linuxize.com/post/how-to-mount-and-unmount-file-systems-in-linux/
+
+# umount DIRECTORY
+# umount DEVICE_NAME
+
+umount temp_test.dir
