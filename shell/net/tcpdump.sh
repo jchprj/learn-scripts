4e41a200ea7d6adb86390ceac38b23eb6a9bd2c9
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-02-25 13:55:56 +0800

    tcpdump

--- shell/net/tcpdump.sh
+++ shell/net/tcpdump.sh
@@ -9,3 +9,8 @@ sudo tcpdump -i any port 22
 # https://dzone.com/articles/tcpdump-learning-how-read-udp
 # UDP:
 # tcpdump -i lo udp port 8125 -vv -X
+
+
+# https://stackoverflow.com/questions/10898358/how-can-i-dump-only-outgoing-ip-packets-in-tcpdump/49101493
+# sudo tcpdump src ...
+# sudo tcpdump dst ...
