3acbf38b55660a78cc786b6b3913e11deddaa20f
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-03-27 12:06:45 +0800

    openconnect

---
+++ shell/net/openconnect.sh
@@ -0,0 +1,22 @@
+# http://www.infradead.org/openconnect/
+# OpenConnect is an SSL VPN client initially created to support Cisco's AnyConnect SSL VPN. 
+
+# https://en.wikipedia.org/wiki/OpenConnects
+# https://gitlab.com/openconnect/openconnect
+# Mirror: https://github.com/openconnect/openconnect
+
+
+# https://cs.uwaterloo.ca/twiki/view/CF/OpenConnect
+# Examples(may need sudo)
+# This will prompt for the watiam password for the given userid
+openconnect -u userid -b cn-vpn.uwaterloo.ca
+# This will prompt for both userid and password
+openconnect -b cn-vpn.uwaterloo.ca
+
+
+
+
+# Cisco AnyConnect on Raspbian [duplicate]
+# https://unix.stackexchange.com/questions/404920/cisco-anyconnect-on-raspbian
+# Raspberry Pi 3 How to connect to work VPN
+# https://raspberrypi.stackexchange.com/questions/65984/raspberry-pi-3-how-to-connect-to-work-vpn
