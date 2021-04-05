68e2df7b321824207146b7a73fe3d271fa1c418a
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-04-05 11:33:18 +0800

    xargs

--- shell/flow/xargs.sh
+++ shell/flow/xargs.sh
@@ -6,3 +6,12 @@ echo " Bash Scripting Language " | xargs
 
 # -n, --max-args=MAX-ARGS      use at most MAX-ARGS arguments per command line
 echo a b|xargs -n1 echo
+
+
+# https://stackoverflow.com/questions/14639206/how-can-i-pass-all-arguments-with-xargs-in-middle-of-command-in-linux
+echo prefix | xargs -I {} echo {} post
+echo prefix | xargs -I % echo % post
+
+
+# https://stackoverflow.com/questions/6958689/running-multiple-commands-with-xargs
+echo abc | xargs -I {} sh -c "echo {} && echo more {}"
