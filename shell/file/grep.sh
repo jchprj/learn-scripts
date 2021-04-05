e48ec0101e55dd700dd26d43312db7b0383d33e8
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-04-05 11:33:24 +0800

    grep

--- shell/file/grep.sh
+++ shell/file/grep.sh
@@ -1,3 +1,9 @@
 # https://stackoverflow.com/questions/1898553/return-a-regex-match-in-a-bash-script-instead-of-replacing-it
 echo "TestT100String" | grep -o '[0-9]\+'       # 100
 echo "TestT100String" | grep '[0-9]\+'          # TestT100String
+
+
+
+
+# https://unix.stackexchange.com/questions/223503/how-to-use-grep-when-file-does-not-contain-the-string
+grep -q query_text * -r && echo true || echo false
