cf5f2139f6a561a36535ffaa862c8c6e157e1e65
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-04-14 14:10:30 +0800

    Java Optional

---
+++ java/flow/optional.java
@@ -0,0 +1,15 @@
+// https://www.amitph.com/avoid-nullpointerexception-using-java-8-optional/
+import java.util.Optional;
+
+public class test {
+    public static void main (String[] args) {
+        String a = "null";
+        Optional.ofNullable(a.length()).ifPresent(Integer::valueOf);
+        a = null;
+        Optional<String> optAddress = Optional.ofNullable(a);
+        String address = optAddress.orElse("null");
+        System.out.println(address);                                        // null
+        Optional.ofNullable(a).ifPresent(System.out::println);              // No execution to System.out::println
+        Optional.ofNullable(a.length()).ifPresent(Integer::valueOf);        // Throw NPE
+    }
+}
