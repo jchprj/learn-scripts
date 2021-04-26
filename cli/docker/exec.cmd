43c1c9a1d61a72e8a8168bb6637f635420499a92
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-04-26 20:22:42 +0800

    Docker cli

---
+++ cli/docker/exec.cmd
@@ -0,0 +1,2 @@
+echo Run command in an named and running container
+docker exec -it temp_test echo Hello world!
--- cli/docker/run.cmd
+++ cli/docker/run.cmd
@@ -1,1 +1,20 @@
+echo -i, --interactive                    Keep STDIN open even if not attached
+echo -t, --tty                            Allocate a pseudo-TTY
+echo -d, --detach                         Run container in background and print container ID
+echo     --name string                    Assign a name to the container
+echo -e, --env list                       Set environment variables
+echo     --rm                             Automatically remove the container when it exits
+
+
+echo Run with environment variables and remove after finished
 docker run --rm -it -p 8080:8080/tcp -e HTTP_PROXY=http://ip:port -e HTTPS_PROXY=http://ip:port temp_test:latest
+
+echo Run with name
+docker run --name temp_test temp_test:latest
+echo Run with name and enter terminal but stop container when exit
+docker run --name temp_test -it temp_test:latest /bin/sh
+echo Run with name and keep in background(detached mode)
+docker run --name temp_test -d -it temp_test:latest /bin/sh
+
+echo Run in detached mode
+docker run -d -p 8000:8000 temp_test:latest
