6ccf7f2908a3f58bd1b1fab02d60df195dc9e178
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-07-03 16:20:25 +0800

    Java jackson module

---
+++ kotlinscript/library_utils/jackson.kts
@@ -0,0 +1,12 @@
+#!/usr/bin/env kscript
+// https://stackoverflow.com/questions/44870961/how-to-map-a-json-string-to-kotlin-map
+@file:DependsOn("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.4")
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+val jsonString = """{"a":12, "b":[true, false]}"""
+val map = ObjectMapper().readValue<MutableMap<Any, Any>>(jsonString)
+println(map)            // {a=12, b=[true, false]}
+println(map["b"])       // [true, false]
+// println(map["b"][1])    // error: no get method providing array access
