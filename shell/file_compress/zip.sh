c020f627a7e00197f1bedd639ee04b748f007457
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-08-26 21:52:59 +0800

    zip -j

--- shell/file_compress/zip.sh
+++ shell/file_compress/zip.sh
@@ -1,1 +1,8 @@
 zip temp_test.zip file1
+
+# https://stackoverflow.com/questions/9710141/create-zip-file-and-ignore-directory-structure
+# -j
+# --junk-paths
+#           Store just the name of a saved file (junk the path), and do  not
+#           store  directory names. By default, zip will store the full path
+#           (relative to the current directory).
