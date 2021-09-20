0eeaa30891604750ee904a0a94f085c11c50fd12
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-09-21 00:41:39 +0800

    sshuttle

---
+++ cli/ssh/sshuttle.sh
@@ -0,0 +1,18 @@
+# https://sshuttle.readthedocs.io/en/stable/usage.html
+
+sshuttle -r username@sshserver 0/0  # short cut for 0.0.0.0/0(Forward all traffic)
+
+
+# https://github.com/sshuttle/sshuttle/issues/334
+# Bug(merged): Name cannot be resolved if it contains - 
+
+
+# https://github.com/sshuttle/sshuttle
+# Run latest code
+
+# virtualenv -p python3 /tmp/sshuttle
+# . /tmp/sshuttle/bin/activate
+# git clone https://github.com/sshuttle/sshuttle.git
+# cd sshuttle
+# ./setup.py install
+
