b4535c12e47af60e7db9473c71ad3c0895213816
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-09-30 19:23:29 +0800

    uniq

--- shell/file/uniq.sh
+++ shell/file/uniq.sh
@@ -1,10 +1,17 @@
 sort file1 | uniq
 
 
+echo
 # https://www.cyberciti.biz/faq/unix-linux-shell-removing-duplicate-lines/
 sort file1 | uniq -u
 
 
+echo
 # https://www.computerhope.com/unix/uuniq.htm
 # uniq does not detect repeated lines unless they are adjacent. You may want to sort the input first, or use sort -u instead of uniq.
 uniq file1          # will contain multiple 2
+
+
+echo
+# https://www.unix.com/shell-programming-and-scripting/136468-group-using-shell-script.html
+sort file1|uniq -c
