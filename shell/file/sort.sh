159619117fddf3ca2649f018c837173f360bcd07
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-10-17 15:12:58 +0800

    sort

--- shell/file/sort.sh
+++ shell/file/sort.sh
@@ -3,6 +3,9 @@ sort file1
 # only unique lines
 sort -u file1
 
+cp file1 temp_test
+# unique file in place
+sort -u temp_test -o temp_test
 
 # https://stackoverflow.com/questions/9245638/select-random-lines-from-a-file-in-bash
 sort -R file1 | head -n 3 >temp_test
