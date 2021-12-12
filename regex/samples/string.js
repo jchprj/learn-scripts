c5233e1349e41bdb69f26624a34bcd7cc6422581
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-12-12 15:26:23 +0000

    regex

--- regex/samples/string.js
+++ regex/samples/string.js
@@ -5,3 +5,11 @@
 
 // ^abc$	start / end of the string
 // (?!abc)	negative lookahead
+
+
+// https://stackoverflow.com/questions/24636814/regular-expression-only-match-if-string-ends-with-target
+/8/g        // All 8
+
+/8$/m       // Only last 8 in one line
+
+/8$/        // Nothing
