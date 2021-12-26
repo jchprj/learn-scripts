30546af75a121a3870f24dc49c70a6eaf9c02ea9
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-12-26 22:01:07 +0800

    Python 2

--- python/python2.py
+++ python/python2.py
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/questions/68382662/how-to-install-pip-on-python-2-7-in-2021
+# PyPi has discontinued support for Python <2.7.9 as of May 6th 2021
+
 def greet(name):
     print "Hello, {0}!".format(name)
 print "What's your name?"
