0cfd728b789730320cf5e03508205ee0efc4c12c
Jue Chen <3330584+jchprj@users.noreply.github.com>
2022-01-10 20:21:28 +0800

    Terraform

--- terraform/local/main.tf
+++ terraform/local/main.tf
@@ -8,15 +8,15 @@ variable "filename" {
 
 # https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
 resource "local_file" "foo1" {
-  content     = "foo!"
-  filename    = "${var.filename}"
+  content  = "foo!"
+  filename = "${var.filename}"
 }
 
 
 # https://www.terraform.io/docs/language/data-sources/index.html
 # https://registry.terraform.io/providers/hashicorp/local/latest/docs/data-sources/file
 data "local_file" "foo" {
-    filename = "local_file"
+  filename = "local_file"
 }
 
 output "local_file_content" {
--- terraform/loop/nested.tf
+++ terraform/loop/nested.tf
@@ -2,7 +2,7 @@ provider "local" {
 }
 
 variable "name_counts" {
-  type    = map(number)
+  type = map(number)
   default = {
     "01" = 1
     "02" = 3
@@ -12,25 +12,47 @@ variable "name_counts" {
 }
 
 locals {
-    # https://www.terraform.io/language/functions/flatten
-    # https://www.terraform.io/language/functions/values
-    expanded_names = flatten(values({
-        # https://www.terraform.io/language/expressions/for
-        # https://www.terraform.io/language/functions/range
-        for name, count in var.name_counts : name => [
-            for i in range(count) : format("temp_test_%s.%02d", name, i)
-        ]
-    }))
+  # https://www.terraform.io/language/functions/flatten
+  # https://www.terraform.io/language/functions/values
+  names_map = {
+    for name, count in var.name_counts : name => [
+      for i in range(count) :
+      {
+        key   = format("%s", name),
+        value = format("temp_test_%s.%02d", name, i)
+      }
+    ]
+  }
+  flatten_names_list = [for index, kv in flatten(values(local.names_map)) : kv.value]
+  flatten_names_map  = { for index, kv in flatten(values(local.names_map)) : index => kv }
+  keys_list          = flatten(keys(local.names_map))
 }
 
-output "hello_world" {
-    value = local.expanded_names
+output "names_map" {
+  value = local.names_map
+}
+
+output "flatten_names_list" {
+  value = local.flatten_names_list
+}
+
+output "flatten_names_map" {
+  value = local.flatten_names_map
+}
+
+output "keys_list" {
+  value = local.keys_list
 }
 
 resource "local_file" "foo3" {
-    # https://www.terraform.io/language/meta-arguments/for_each
-    for_each    = toset(local.expanded_names)
-    content     = "foo!"
-    filename    = "${each.value}"
+  # https://www.terraform.io/language/meta-arguments/for_each
+  # for_each    = local.flatten_names_map
+  # https://stackoverflow.com/questions/63620298/terraform-how-to-use-conditional-if-in-for-each-into-map-object
+  for_each = {
+    for index, kv in local.flatten_names_map : index => kv
+    if 1 > 0
+  }
+  content  = "foo!"
+  filename = "${each.value.value}"
 }
 
