1ea2b8c442f1d7f649de9608b21d8f6b3b42349b
Jue Chen <3330584+jchprj@users.noreply.github.com>
2022-03-06 16:47:01 +0000

    Linux process commands

---
+++ shell/process/kill.sh
@@ -0,0 +1,15 @@
+# https://stackoverflow.com/questions/6381229/how-to-kill-all-processes-matching-a-name
+ps aux | grep -ie amarok | awk '{print $2}' | xargs kill -9
+
+
+
+# https://www.linuxquestions.org/questions/linux-newbie-8/what-is-hup-signal-how-to-send-one-to-squid-267536/
+# Kill is actually slightly mis-named, it doesn't necessarily terminate a process, it sends it a signal of a particular type. A signal is just a way of notifying a running program that something has happened, like an invalid memory access or an interrupted system call or whatever (see kill -l for a list of all signals). Anyhow, to make a very long story short, to send SIGHUP to squid, you would do
+
+kill -HUP <pid of squid>
+
+# or, to save yourself from having to look up the PID:
+# killall -HUP squid
+
+# https://stackoverflow.com/questions/19052354/sighup-for-reloading-configuration
+# SIGHUP for reloading configuration
---
+++ shell/process/ps.sh
@@ -0,0 +1,19 @@
+ps --help all
+
+ps -ef
+    #  -A, -e               all processes
+    #  -f                   full-format, including command lines
+
+ps aux
+    #  a                   all with tty, including other users
+    #  u                   user-oriented format
+    #  x                   processes without controlling ttys
+
+# https://askubuntu.com/questions/88972/how-to-get-from-terminal-total-number-of-threads-per-process-and-total-for-al
+# To get the number of threads for a given pid:
+ps -o nlwp <pid>
+# To the get the sum of all threads running in the system:
+ps -eo nlwp | tail -n +2 | awk '{ num_threads += $1 } END { print num_threads }'
+
+# https://unix.stackexchange.com/questions/74185/how-can-i-prevent-grep-from-showing-up-in-ps-results
+ps aux | grep "[f]nord"
