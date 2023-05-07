af03508e0aec6cbefb7041877a001bf371bae0f8
Jue Chen <3330584+jchprj@users.noreply.github.com>
2023-05-07 19:13:49 +0800

    GCloud

---
+++ +cli/gcloud/storage.cmd
@@ -0,0 +1,7 @@
+echo https://cloud.google.com/storage/docs/gsutil
+echo https://cloud.google.com/storage/docs/gsutil/commands/cp
+echo -r option to copy an entire directory tree
+echo -m multi-threaded/multi-processing
+
+gsutil ls
+gsutil cp *.txt gs://my-bucket
